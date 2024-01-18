06e30b8246301cbecc7b1674aec33bc4188d2434
morpheus-30 <konnichiwasekai7@gmail.com>
2024-01-19 00:19:11 +0530

    misc

--- momento/lib/constants.dart
+++ momento/lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/daily_trivia/Completed.dart';
 
 const brown = Color(0xFFAB6D26);
 const darkBrown = Color(0xFF674117);
@@ -10,3 +11,4 @@ class CustomStrings {
   static const String clientId = "7adcf5b89e344059b05104d37b13d0d8";
   static const String clientSecret = "a17de832dbca425ba652831dfed97a5c";
 }
+
